b026e1d2a0544e54c423337778ed44714f8c871f
MaheKarim <mahekarim@gmail.com>
2019-02-26 00:29:30 +0600

    Back-end Update

---
+++ resources/views/v2/back-end/includes/footer/footer.blade.php
@@ -0,0 +1,27 @@
+<footer class="footer">
+    <div class="d-sm-flex justify-content-center justify-content-sm-between">
+      <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2019 <a href="https://www.mahekarim.com/" target="_blank">Mahe Karim</a>. All rights reserved.</span>
+      <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="mdi mdi-heart text-danger"></i></span>
+    </div>
+  </footer>
+  <!-- partial -->
+</div>
+<!-- main-panel ends -->
+</div>
+<!-- page-body-wrapper ends -->
+</div>
+<!-- container-scroller -->
+
+<!-- plugins:js -->
+<script src="{{ asset('/') }}admin/vendors/js/vendor.bundle.base.js"></script>
+<script src="{{ asset('/') }}admin/vendors/js/vendor.bundle.addons.js"></script>
+<!-- endinject -->
+<!-- Plugin js for this page-->
+<!-- End plugin js for this page-->
+<!-- inject:js -->
+<script src="{{ asset('/') }}admin/js/off-canvas.js"></script>
+<script src="{{ asset('/') }}admin/js/misc.js"></script>
+<!-- endinject -->
+<!-- Custom js for this page-->
+<script src="{{ asset('/') }}admin/js/dashboard.js"></script>
+<!-- End custom js for this page-->
